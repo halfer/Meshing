1774d10f702128ac98bdb7f2441c8546f8401a0c
Jon Hinks <projects.ws@jondh.me.uk>
2011-09-19 01:32:24 +0100

    Let help command offer individual command syntax

--- lib/P2P/Console/Command/Help.php
+++ lib/P2P/Console/Command/Help.php
@@ -24,11 +24,95 @@ class P2P_Console_Command_Help extends P2P_Console_Base implements P2P_Console_I
 	{
 	}
 
+	/**
+	 * Provides either a command listing or specific command help
+	 * 
+	 * @todo Expand internal help format to specify whether a boolean switch is mandatory
+	 */
 	public function run()
 	{
 		// Reads the commands dynamically from the file system
 		$commands = P2P_Console_Utils::getCommands();
 
+		if (array_key_exists(0, $this->argv))
+		{
+			$command = $this->argv[0];
+			$className = array_search($command, $commands);
+			if ($className)
+			{
+				$class = new $className;
+				$opts = $class->getOpts();
+				echo "Syntax: ./meshing $command ";
+				if ($opts)
+				{
+					$syntax = array();
+
+					// The key is in the form --name|n=s (mandatory param) or --name|n-s (optional)
+					foreach ($opts as $key => $help)
+					{
+						$parts = explode('|', $key);
+						$longForm = $parts[0];
+						$optional = $type = false;
+						$syntax[$longForm] = $help;
+						if (array_key_exists(1, $parts))
+						{
+							$optional = substr($parts[1], 1, 1) == '-';
+							$type = $this->convertType(
+								substr($parts[1], 2, 1)
+							);
+							$type = $type ? '<' . $type . '>' : '';
+						}
+						else
+						{
+							$optional = true;
+						}
+
+						// Add a --flag=<type> line
+						echo $optional ? '[' : '';
+						echo '--' . $longForm;
+						echo $type ? '=' . $type : '';
+						echo ' ';
+						echo $optional ? ']' : '';
+					}
+					echo "\nwhere:\n";
+
+					// Print detailed help, line by line
+					foreach ($syntax as $switch => $help)
+					{
+						echo "  --" . $switch . str_repeat(' ', 16 - strlen($switch)) . $help . "\n";
+					}
+				}
+				else
+				{
+					echo "\n";
+				}
+			}
+			else
+			{
+				throw new Zend_Console_Getopt_Exception(
+					'Command not recognised (use no arguments to see list)'
+				);
+			}
+		}
+		else
+		{
+			$this->listCommands($commands);
+		}
+	}
+
+	protected function convertType($typeCode)
+	{
+		switch ($typeCode)
+		{
+			case 'i': return 'integer';
+			case 's': return 'string';
+		}
+
+		return null;
+	}
+
+	protected function listCommands($commands)
+	{
 		// Print a hint for all commands
 		foreach ($commands as $className => $command)
 		{
