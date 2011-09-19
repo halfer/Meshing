377070a0aa76c97e5909427f002536fe1dbdc93c
Jon Hinks <projects.ws@jondh.me.uk>
2011-09-19 01:25:53 +0100

    Rename shell command from "p2p" to "meshing"

--- demos/jobs.sh
+++ demos/jobs.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Only run this file from the project root
-if [ ! -f "p2p" ]; then
-	echo 'Error: must be run from the P2P root folder'
+if [ ! -f "meshing" ]; then
+	echo 'Error: must be run from the Meshing root folder'
 	exit 1
 fi
 
 # Rebuild system tables on db
-./p2p system:build --database --verbose
+./meshing system:build --database --verbose
 
 # Add new connections
-./p2p connection:add --name jobs_conn_1 --adaptor pgsql --database p2p_jobs --host localhost --user jon
-./p2p connection:add --name jobs_conn_2 --adaptor pgsql --database p2p_jobs --host localhost --user jon
+./meshing connection:add --name jobs_conn_1 --adaptor pgsql --database db_jobs_1 --host localhost --user jon
+./meshing connection:add --name jobs_conn_2 --adaptor pgsql --database db_jobs_2 --host localhost --user jon
 
 # Add a schema
-./p2p schema:add --name jobs --file demos/jobs.xml
+./meshing schema:add --name jobs --file demos/jobs.xml
 
 # @todo Add some nodes
-# ./p2p node:add --name node_jobs_1 --connection jobs_conn_1 --schema jobs
-# ./p2p node:add --name node_jobs_2 --connection jobs_conn_2 --schema jobs
+# ./meshing node:add --name node_jobs_1 --connection conn_jobs_1 --schema jobs
+# ./meshing node:add --name node_jobs_2 --connection conn_jobs_2 --schema jobs
 
 # @todo Add some trust
