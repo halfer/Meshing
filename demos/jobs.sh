02116effccdd260eff2e87012a72ee422b9cd354
Jon Hinks <projects.ws@jondh.me.uk>
2011-09-24 11:14:00 +0100

    Minor naming changes to jobs build script

--- demos/jobs.sh
+++ demos/jobs.sh
@@ -10,14 +10,14 @@ fi
 ./meshing system:build --database --verbose
 
 # Add new connections
-./meshing connection:add --name jobs_conn_1 --adaptor pgsql --database db_jobs_1 --host localhost --user jon
-./meshing connection:add --name jobs_conn_2 --adaptor pgsql --database db_jobs_2 --host localhost --user jon
+./meshing connection:add --name conn_jobs_1 --adaptor pgsql --database db_jobs_1 --host localhost --user jon
+./meshing connection:add --name conn_jobs_2 --adaptor pgsql --database db_jobs_2 --host localhost --user jon
 
 # Add a schema
 ./meshing schema:add --name jobs --file demos/jobs.xml
 
 # @todo Add some nodes
-# ./meshing node:add --name node_jobs_1 --connection conn_jobs_1 --schema jobs
+./meshing node:add --name node_jobs_1 --connection conn_jobs_1 --schema jobs
 # ./meshing node:add --name node_jobs_2 --connection conn_jobs_2 --schema jobs
 
 # @todo Add some trust
