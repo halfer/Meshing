2671fcb3c86e98b29e5587cc9b426fa486b19413
Jon Hinks <projects.ws@jondh.me.uk>
2011-09-24 13:18:15 +0100

    Simple implementation for schema:list

--- lib/P2P/Console/Command/Schema/List.php
+++ lib/P2P/Console/Command/Schema/List.php
@@ -5,7 +5,39 @@
  *
  * @author jon
  */
-class P2P_Console_Command_Schema_List extends P2P_Console_Stub implements P2P_Console_Interface
+class P2P_Console_Command_Schema_List extends P2P_Console_Base implements P2P_Console_Interface
 {
-	
+	public function getDescription()
+	{
+		return 'Lists the currently registered schemas';
+	}
+
+	public function getOpts()
+	{
+		return array();
+	}
+
+	public function preRunCheck()
+	{
+		
+	}
+
+	public function run()
+	{
+		P2P_Utils::initialiseDb();
+
+		$outFormat = '%-15s';
+		$this->ruleOff($lineLength = 15);
+		echo sprintf($outFormat, 'Name', 'Schema', 'Connection') . "\n";
+		$this->ruleOff($lineLength);
+
+		/* @var $schema P2PSchema */
+		foreach (P2PSchemaPeer::doSelect(new Criteria()) as $schema)
+		{
+			echo sprintf(
+				$outFormat,
+				$schema->getName()
+			) . "\n";
+		}
+	}
 }
