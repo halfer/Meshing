fabef52200fe54f1750549cdd14bd3266fc268f3
Jon Hinks <projects.ws@jondh.me.uk>
2011-10-06 21:59:55 +0100

    Completed first set of tests for Meshing_Schema_Element
    Closes issue #17.

--- tests/unit/schema/element_test.php
+++ tests/unit/schema/element_test.php
@@ -48,26 +48,87 @@ class SchemaElementTestCase extends UnitTestCase
 
 		// Test the foreign table references as well
 		$ok = true;
-		foreach ($this->xml->xpath('/database/table/foreign-key') as $i => $table)
+		foreach ($this->xml->xpath('/database/table/foreign-key') as $i => $foreignKey)
 		{
-			$tableName = (string) $table['foreignTable'];			
+			$tableName = (string) $foreignKey['foreignTable'];			
 			$ok = $ok && ($tableName == $prefix . $fTableNames[$i]);
 		}
 		$this->assertTrue($ok, 'Checking prefixing on foreign key references');
 	}
 
+	/**
+	 * Checks the package name is set correctly
+	 */
 	public function testPackageName()
-	{
-		
+	{		
+		// First value changes it from empty, second checks it can be changed
+		foreach (array( 'bonjour', 'au_revoir' ) as $packageName)
+		{
+			// Set up random package names on existing tables
+			foreach ($this->xml->xpath('/database/table') as $table)
+			{
+				$table['package'] = 'package_' . rand(1, 1000);
+			}
+
+			// Reset database-level package name
+			$this->xml->setPackageName($packageName);		
+			$post = $this->getAttribute($this->xml, 'package');
+			$this->assertTrue($packageName == $post, 'Setting schema database package name');
+			
+			// Check that all table-level packages are unset
+			$ok = true;
+			foreach ($this->xml->xpath('/database/table') as $table)
+			{
+				$val = $this->getAttribute($table, 'package');
+				if (!is_null($val))
+				{
+					$ok = false;
+				}
+			}
+			$this->assertTrue($ok, 'Checking all table-level package names are cleared');
+		}
 	}
 
+	protected function getAttribute(Meshing_Schema_Element $xml, $name)
+	{
+		$attribs = (array) $xml->attributes();
+		$value = null;
+		if (array_key_exists('@attributes', $attribs))
+		{
+			if (array_key_exists($name, $attribs['@attributes']))
+			{
+				$value = $attribs['@attributes'][$name];
+			}
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Tests that one table copies ok into another schema
+	 *
+	 * @todo Check that the structure is copied completely & accurately
+	 */
 	public function testInsertTable()
 	{
-		
+		// Grab table to insert
+		$filename = realpath(dirname(__FILE__)) . '/files/insert_table.xml';
+		$this->xml->insertTable($filename);
+
+		// This should exist
+		$newTables = $this->xml->xpath('/database/table[@name="saucer_sightings"]');
+		$this->assertTrue((boolean) $newTables, 'Insert table into existing schema');
+
+		// This should not exist
+		$newTables = $this->xml->xpath('/database/table[@name="badger_sightings"]');
+		$this->assertFalse((boolean) $newTables, 'A table that should not exist appears to exist');
 	}
 
 	public function testConnName()
 	{
-		
+		$connName = 'guten_tag';
+		$this->xml->setConnectionName($connName);		
+		$post = $this->getAttribute($this->xml, 'name');
+		$this->assertTrue($connName == $post, 'Setting schema connection name');		
 	}
 }
