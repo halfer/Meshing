287f1b5d652d8cca50a907429821bff030296c21
Jon Hinks <projects.ws@jondh.me.uk>
2011-10-06 15:20:49 +0100

    Add another test, some code rearranging

--- tests/unit/schema/element_test.php
+++ tests/unit/schema/element_test.php
@@ -10,36 +10,64 @@ require_once 'simpletest/autorun.php';
 
 class SchemaElementTestCase extends UnitTestCase
 {
-	public function testCreation()
+	public function setUp()
 	{
-		// Create test XML file
+		// Create test XML object
 		$filename = realpath(dirname(__FILE__)) . '/files/test_schema1.xml';
-		$xml = simplexml_load_file($filename, 'Meshing_Schema_Element');
-		
-		$this->prefixing($xml);
-		// @todo setPackageName
-		// @todo insertTable
-		// @todo setConnectionName
-	}
+		$this->xml = simplexml_load_file($filename, 'Meshing_Schema_Element');
+    }
 
-	public function prefixing(Meshing_Schema_Element $xml)
+	public function testPrefixing()
 	{
 		// Grab all the table names
 		$tableNames = array();
-		foreach ($xml->xpath('/database/table') as $i => $table)
+		foreach ($this->xml->xpath('/database/table') as $table)
 		{
 			$tableNames[] = (string) $table['name'];
 		}
 		
-		$prefix = 'hello';
-		$xml->prefixTablesManually($prefix);
+		// Grab all the foreign table names
+		$fTableNames = array();
+		foreach ($this->xml->xpath('/database/table/foreign-key') as $i => $table)
+		{
+			$fTableNames[] = (string) $table['foreignTable'];
+		}
 		
+		// Do the prefixing op
+		$prefix = 'hello';
+		$this->xml->prefixTablesManually($prefix);
+
+		// Test the table changes first
 		$ok = true;
-		foreach ($xml->xpath('/database/table') as $i => $table)
+		foreach ($this->xml->xpath('/database/table') as $i => $table)
 		{
 			$tableName = (string) $table['name'];
 			$ok = $ok && ($tableName == $prefix . $tableNames[$i]);
 		}
 		$this->assertTrue($ok, 'Checking table prefixing');
+
+		// Test the foreign table references as well
+		$ok = true;
+		foreach ($this->xml->xpath('/database/table/foreign-key') as $i => $table)
+		{
+			$tableName = (string) $table['foreignTable'];			
+			$ok = $ok && ($tableName == $prefix . $fTableNames[$i]);
+		}
+		$this->assertTrue($ok, 'Checking prefixing on foreign key references');
+	}
+
+	public function testPackageName()
+	{
+		
+	}
+
+	public function testInsertTable()
+	{
+		
+	}
+
+	public function testConnName()
+	{
+		
 	}
 }
