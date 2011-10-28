69d7f26fb1e0b565317688630a826c6a09dc01d9
Jon Hinks <projects.ws@jondh.me.uk>
2011-10-28 11:01:23 +0100

    Some simple tests to check class building works

--- tests/unit/propel/propel_test.php
+++ tests/unit/propel/propel_test.php
@@ -20,6 +20,29 @@ class PropelGeneralTestCase extends UnitTestCase
 	public function setUp()
 	{
 		$this->projectRoot = realpath(dirname(__FILE__) . '/../../..');
+		$this->paths = Meshing_Utils::getPaths();
+		$this->modelDir = $this->projectRoot . $this->paths->getPathModelsNodes();
+
+		// Delete anything in the model folder
+		if (file_exists($this->modelDir))
+		{
+			// @todo Convert this to PHP, so as to avoid OS-specific call
+			$isWindows = (
+				(PHP_OS == 'WIN32') ||
+				(PHP_OS == 'WINNT') ||
+				(PHP_OS == 'Windows')
+			);
+			if (!$isWindows)
+			{
+				$command = 'rm -rf ' . $this->modelDir . '/*';
+				$return = null;
+				@system($command, $return);
+				if ($return)
+				{
+					trigger_error('Failed to delete model folder', E_USER_WARNING);
+				}
+			}
+		}
 	}
 
 	/**
@@ -31,19 +54,35 @@ class PropelGeneralTestCase extends UnitTestCase
 	public function testClassBuilder()
 	{
 		$rt = $this->projectRoot;
-		$paths = Meshing_Utils::getPaths();
 
-		$extraPropsFile = $rt . $paths->getPathDbConfig() . '/build.properties';
-		$schemaDir = $rt . $paths->getPathDbConfig();
-		$modelDir = $rt . $paths->getPathModelsNodes();
+		$extraPropsFile = $rt . $this->paths->getPathDbConfig() . '/build.properties';
+		$schemaDir = $rt . $this->paths->getPathDbConfig();
 
 		$task = new Meshing_Propel_ClassBuilder();
 		$task->addPropertiesFile($extraPropsFile);
 		$task->addSchemas($schemaDir, 'test_schema1.xml');
-		$task->setOutputDir($modelDir);
-
+		$task->setOutputDir($this->modelDir);
 		$task->run();
-		
-		$this->assertTrue(true);
+
+		$this->package = 'test-db';
+
+		// Find these classes
+		$classes = array(
+			'Event', 'EventPeer', 'EventQuery', 'Organiser', 'OrganiserPeer', 'OrganiserQuery'
+		);
+		foreach ($classes as $class)
+		{
+			$this->assertTrue(
+				$this->classExists($class),
+				'Checking generated class `' . $class . '` exists'
+			);
+		}
+	}
+
+	protected function classExists($model)
+	{
+		return file_exists(
+			$this->modelDir . '/' . $this->package . '/' . $model . '.php'
+		);
 	}
 }
