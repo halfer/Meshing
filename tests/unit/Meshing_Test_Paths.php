0bdabe6c21eaa466697ba007dfbf06223fb5310d
Jon Hinks <projects.ws@jondh.me.uk>
2011-10-28 15:19:33 +0100

    Renamed 'system' folder in test database folder
    The old name implies that (a) we're testing system db stuff, which isn't
    true, and also that (b) we're only testing non-modified schemas i.e.
    items that will not go through the fixup process. In fact, we'll be testing
    both from here - hence rename.

--- tests/unit/Meshing_Test_Paths.php
+++ tests/unit/Meshing_Test_Paths.php
@@ -7,6 +7,20 @@
  */
 class Meshing_Test_Paths extends Meshing_Paths
 {
+	/**
+	 * Returns the test model config folder
+	 * 
+	 * Renamed from 'system' as that name implies it is only for schemas that do not
+	 * go through the fixup process. In our test systems, we use this to test both
+	 * fixed and unmodified schemas.
+	 * 
+	 * @return string
+	 */
+	public function getPathDbConfig()
+	{
+		return $this->getGeneratedPath('/database/config');		
+	}
+
 	protected function getGeneratedPath($path)
 	{
 		return '/tests' . $path;
