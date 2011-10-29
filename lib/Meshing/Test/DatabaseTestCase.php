47dcd1b2ec9ba8c368e57699ef2698f626c36029
Jon Hinks <projects.ws@jondh.me.uk>
2011-10-29 12:24:29 +0100

    More test renamed, added forgotten parent class change

--- lib/Meshing/Test/DatabaseTestCase.php
+++ lib/Meshing/Test/DatabaseTestCase.php
@@ -1,28 +1,43 @@
 <?php
 
 /**
- * Used to set up a test model
+ * Used to set up a database test
  *
  * @author jon
  */
-class Meshing_Test_ModelTestCase extends UnitTestCase
+class Meshing_Test_DatabaseTestCase extends UnitTestCase
 {
-	public function setUp()
+	protected function deleteFolderContents($folder, $purpose)
 	{
-		$projectRoot = Meshing_Utils::getProjectRoot();
-		// if (!model exists)
-		// {
-		//		do set up
-		// }
-		
-		register_shutdown_function(array($this, 'deleteModel'));
-	}
+		if (file_exists($folder))
+		{
+			// Preserve these, they are part of the folder structure
+			$preserve = array('.ignore');
+			
+			// Delete contents of specified folder
+			$directory = new RecursiveDirectoryIterator($folder);
+			$iterator = new RecursiveIteratorIterator(
+				$directory,
+				RecursiveIteratorIterator::CHILD_FIRST
+			);
+			
+			$success = true;
+			foreach ($iterator as $path)
+			{
+				$name = $path->__toString();
+				if (!in_array($name, $preserve))
+				{
+					$success = ($path->isDir() ? @rmdir($name) : @unlink($name)) && $success;
+				}
+			}
 
-	public function deleteModel()
-	{
-		// if (model still exists)
-		// {
-		//		delete model
-		// }
+			if (!$success)
+			{
+				trigger_error(
+					"Failed to delete contents of $purpose folder",
+					E_USER_WARNING
+				);
+			}
+		}
 	}
 }
